ca232b06003e8c08c38db71d7b29b5f5cbab2551
thesourceuser <108767916+thesourceuser@users.noreply.github.com>
2022-07-06 19:07:38 -0400

    Create code.lua

---
+++ code.lua
@@ -0,0 +1,24 @@
+if _G.sourcelib then return _G.sourcelib end
+
+link = "https://raw.githubusercontent.com/%s/%s/code.lua"
+paste = "https://pastebin.com/raw/%s"
+
+local module = {}
+_G.sourcelib = module
+
+module.__index = module
+module.__lib = {}
+
+function module:require(package, version, update)
+    local link = link:format(package, version or "main")
+    if self.__lib[link] and update ~= true then return self.__lib[link] end
+
+    self.__lib[link] = loadstring(game:HttpGet(link))
+    return self.__lib[link]
+end
+
+function module:test(pin)
+    return loadstring(game:HttpGet(paste:format(pin)))
+end
+
+return setmetatable({}, module)
